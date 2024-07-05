21eea26eb10d14641d30896398c574aaa59f6f5e
SamQueen <samqueenbusiness@gmail.com>
2024-07-05 16:45:43 -0700

    UPdataed socket production url

--- lib/socket.tsx
+++ lib/socket.tsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 const inProduction = process.env.NODE_ENV === 'production';
-const url = (inProduction) ? 'https://www.pictureper.com' : 'http://localhost:5000';
+const url = (inProduction) ? 'https://api.pictureper.com' : 'http://localhost:5000';
 
 export const socket = io(url);
