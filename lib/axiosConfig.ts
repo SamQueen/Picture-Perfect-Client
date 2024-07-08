65aaf332866c94652164244f4247ffb19b96e61e
SamQueen <samqueenbusiness@gmail.com>
2024-07-07 17:51:07 -0700

    Update production url

--- lib/axiosConfig.ts
+++ lib/axiosConfig.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 const inProduction = process.env.NODE_ENV === "production";
 
 const devUrl = 'http://localhost:5000';
-const productionUrl = 'https://api.pictureper.com';
+//const productionUrl = 'https://api.pictureper.com';
+const productionUrl = 'https://www.pictureper.com';
 
 const instance = axios.create({
     baseURL: inProduction ? productionUrl : devUrl, // Your API base URL
